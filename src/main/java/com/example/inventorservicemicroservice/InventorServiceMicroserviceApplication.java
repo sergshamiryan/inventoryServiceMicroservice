69445bd7427fadba0aee9d6ab4cc2b0505caa45b
Sergey <sergshamiryan@gmail.com>
2023-05-30 06:22:25 +0400

    Commit

---
+++ src/main/java/com/example/inventorservicemicroservice/InventorServiceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorservicemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorServiceMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorServiceMicroserviceApplication.class, args);
+    }
+
+}
